27f838c281e3d6a63b497fff2e3b2b1a573ecba6
Tara Johnson <tara@madface.com>
2018-06-18 19:34:43 -0700

    Resolving merge conflicts

--- 10-calculate-average/solution.js
+++ 10-calculate-average/solution.js
@@ -1,9 +1,5 @@
 'use strict';
 
 function find_average(array) {
-<<<<<<< HEAD
-  return array.reduce((a,b) => a + b)/array.length;
-=======
-  return array.reduce((a, b) => (a + b)) / array.length;
->>>>>>> 30d7a4dd97ac0003fc9f254c492fc6b5561a1406
+  return array.reduce((a,b) => a + b) / array.length;
 }
