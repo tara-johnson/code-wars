93c50050c7985659a5ce1a194b18761ed526411b
Tara Johnson <tara@madface.com>
2018-06-21 16:02:51 -0700

    Added solution code

--- 13-mutate-my-strings/solution.js
+++ 13-mutate-my-strings/solution.js
@@ -1,5 +1,14 @@
 'use strict';
 
-function mutateMyStrings(stringOne, stringTwo){
-
+function mutateMyStrings(str1, str2) {
+  let outArr = [str1, '\n'];
+  let arr1 = str1.split('');
+  let arr2 = str2.split('');
+  for (var i = 0; i < str2.length; i++) {
+    if (arr2[i] !== arr1[i]) {
+      arr1[i] = arr2[i];
+      outArr.push(arr1.join(''), '\n');
+    }
+  }
+  return outArr.join('');
 }
